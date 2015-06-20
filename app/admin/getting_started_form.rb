69134e006b2622bd8afc8cc9a6f09e70ff94894b
Myles Cowper-Coles <myles@farmdrop.co.uk>
2015-06-20 16:06:55 +0100

    addmin and customins the getting started form in admin

--- app/admin/getting_started_form.rb
+++ app/admin/getting_started_form.rb
@@ -22,4 +22,22 @@ ActiveAdmin.register GettingStartedForm do
     actions
   end
 
+  show do |form|
+    attributes_table do
+
+      row :id
+      row :business_name
+      row :number_of_employees
+      row :phone
+      row :user 
+      row :website_terms_of_use do status_tag((form.website_terms_of_use == '1') ? 'yes' : 'no') end
+      row :acceptable_use_policy do status_tag((form.acceptable_use_policy == '1') ? 'yes' : 'no') end
+      row :privacy_and_cookies_policy do status_tag((form.privacy_and_cookies_policy == '1') ? 'yes' : 'no') end
+      row :other 
+      row :created_at
+
+    end
+  end
+
+
 end
