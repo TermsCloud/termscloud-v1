c848bf5baf2e82507f08d064978935057000a2b2
Myles Cowper-Coles <myles@farmdrop.co.uk>
2015-06-20 15:57:11 +0100

    moving clearance views into the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
-  ActiveAdmin.routes(self)
   root 'homepage#index'
+
+  ActiveAdmin.routes(self)
+
 
   get 'homepage/index'
   post 'homepage/register_user'
   get 'homepage/get_started'
   post 'homepage/get_started', to: 'homepage#submit_getting_started'
-
 
 end
