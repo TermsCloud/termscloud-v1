0911c34e298ca3aed612b6914077a4d492174dbf
Myles Cowper-Coles <myles@farmdrop.co.uk>
2015-06-20 14:08:02 +0100

    styling the get started page

--- app/models/getting_started_form.rb
+++ app/models/getting_started_form.rb
@@ -1,5 +1,5 @@
 class GettingStartedForm< ActiveRecord::Base
-  store_accessor :details, :website_terms_of_use, :aceeptable_use_policy, :privacy_and_cookies_policy, :other
+  store_accessor :details, :website_terms_of_use, :acceptable_use_policy, :privacy_and_cookies_policy, :other
 
   validates :business_name, presence: true
   validates :number_of_employees, presence: true
