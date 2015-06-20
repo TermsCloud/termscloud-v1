7cae82f8a8a412a9519ba83435bdb1cde556b6f0
Myles Cowper-Coles <myles@farmdrop.co.uk>
2015-06-20 10:35:56 +0100

    call to action all styled up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620084657) do
+ActiveRecord::Schema.define(version: 20150620092712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150620084657) do
     t.string   "firstname"
     t.string   "lastname"
     t.string   "website_url"
+    t.boolean  "terms"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
